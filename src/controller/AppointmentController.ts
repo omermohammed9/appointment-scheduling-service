cbf6f4dabc3ebf07e807515c2c7b5a440edd0b27
scorpiraq <96728173+scorpiraq@users.noreply.github.com>
2023-11-21 16:53:06 +0300

    Adding ValidationMiddleWare to routes & Updating server response in controller using customized class

--- src/controller/AppointmentController.ts
+++ src/controller/AppointmentController.ts
@@ -3,6 +3,8 @@ import {DataSource} from "typeorm";
 import AppointmentRepository from "../repository/AppointmentRepository";
 import AppointmentService from "../service/AppointmentService";
 import {Controller, Delete, Get, Next, Patch, Post, Req, Res} from "@decorators/express";
+import sendResponse from "../ResponseHelper/ResponseHelper";
+import {validateIdMiddlewareRequest} from "../middleware/ValidationDataMiddleware";
 
 @Controller(`/appointment`)
 class AppointmentController {
@@ -19,8 +21,8 @@ class AppointmentController {
         try {
         const appointment = req.body;
         const newAppointment = await this.appointmentService.createAppointment(appointment);
-            return res.status(201).json(newAppointment);
-
+            sendResponse(res, 201, newAppointment, 'Appointment created successfully');
+            //return res.status(201).json(newAppointment);
         }catch (err){
             next(err);
         }
@@ -30,40 +32,46 @@ class AppointmentController {
     async getAllAppointments(@Req() req: Request, @Res() res: Response, @Next() next: Function) {
         try {
         const Appointments = await this.appointmentService.findAllAppointments();
-        return res.json(Appointments);
+        sendResponse(res, Appointments ? 200 : 404, Appointments, Appointments? ' ': 'No Appointments found');
+        //return res.json(Appointments);
         }catch (err){
             next(err);
         }
     }
-    @Get(`/getappointment/:id`)
+    @Get(`/getappointment/:id`, [validateIdMiddlewareRequest])
     async getAppoitmentById(@Req() req: Request, @Res() res: Response, @Next() next: Function) {
         try {
-        const id = +req.params.id;
-        const Appoitment = await this.appointmentService.findAppoitmentById(id);
+        const Appoitment = await this.appointmentService.findAppoitmentById(req.validatedId);
+        sendResponse( res, Appoitment ? 200 : 404, Appoitment, Appoitment? ' ': 'No Appoitment found by this id');
         return res.json(Appoitment);
         }catch (err){
             next(err);
         }
     }
-    @Patch(`/updateappointment/:id`)
+    @Patch(`/updateappointment/:id`, [validateIdMiddlewareRequest])
     async updateAppoitment(@Req() req: Request, @Res() res: Response, @Next() next: Function) {
         try {
-        const id = +req.params.id;
-        const updateData = req.body;
-        const Appointment = await this.appointmentService.updateAppoitment(id, updateData);
+        const Appointment = await this.appointmentService.updateAppoitment(req.validatedId, req.body);
+        sendResponse( res, Appointment ? 200 : 404, Appointment, Appointment? ' ': 'No Appoitment found by this id to update');
         return res.json(Appointment);
         }catch (err){
             next(err);
         }
     }
-    @Delete(`/deleteappointment/:id`)
+    @Delete(`/deleteappointment/:id`, [validateIdMiddlewareRequest])
     async deleteAppoitment(@Req() req: Request, @Res() res: Response, @Next() next: Function) {
         try {
-        const id = +req.params.id;
-        const Appoitment = await this.appointmentService.deleteAppoitment(id);
-        return res.json(Appoitment);
-        }catch (err){
-            next(err);
+             await this.appointmentService.deleteAppoitment(req.validatedId);
+        sendResponse( res, 200, null,  'Appoitment deleted successfully');
+        //return res.json(Appoitment);
+        }catch (err: any) {
+            if (err.message === 'Appoitment not found') {
+                sendResponse(res, 404, null, err.message);
+                //res.status(404).json({ message: err.message });
+            } else {
+                sendResponse(res, 500, null, err.message);
+                //res.status(500).json({ message: err.message });
+            }
         }
     }
 }
--- src/routes/appointmentRoutes.ts
+++ src/routes/appointmentRoutes.ts
@@ -1,15 +1,18 @@
 import {Router} from "express";
 import AppointmentController from "../controller/AppointmentController";
 import AppDataSource from "../database/database";
+import {validateIdMiddlewareRequest, validationDataMiddleware} from "../middleware/ValidationDataMiddleware";
+import CreateAppointmentDTO from "../dto/CreateAppointmentDTO";
+import {UpdateAppointmentDTO} from "../dto/UpdateAppointmentDTO";
 
 
 const AppointmentRouter = Router();
 
 const appointmentController = new AppointmentController(AppDataSource);
 
-AppointmentRouter.get(`/getallappointments`, appointmentController.getAllAppointments.bind(appointmentController));
-AppointmentRouter.get(`/getappointment/:id`, appointmentController.getAppoitmentById.bind(appointmentController));
-AppointmentRouter.post(`/create`, appointmentController.createAppointment.bind(appointmentController));
-AppointmentRouter.patch(`/updateappointment/:id`, appointmentController.updateAppoitment.bind(appointmentController));
-AppointmentRouter.delete(`/deleteappointment/:id`, appointmentController.deleteAppoitment.bind(appointmentController));
+AppointmentRouter.get(`/getallappointments`, validateIdMiddlewareRequest, appointmentController.getAllAppointments.bind(appointmentController));
+AppointmentRouter.get(`/getappointment/:id`, validateIdMiddlewareRequest, appointmentController.getAppoitmentById.bind(appointmentController));
+AppointmentRouter.post(`/create`, validationDataMiddleware(CreateAppointmentDTO), appointmentController.createAppointment.bind(appointmentController));
+AppointmentRouter.patch(`/updateappointment/:id`, validationDataMiddleware(UpdateAppointmentDTO), validateIdMiddlewareRequest, appointmentController.updateAppoitment.bind(appointmentController));
+AppointmentRouter.delete(`/deleteappointment/:id`, validateIdMiddlewareRequest, appointmentController.deleteAppoitment.bind(appointmentController));
 export default AppointmentRouter;
